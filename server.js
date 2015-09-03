b8f7b1fecf3250756d50f590a3c3d198c19c6db2
rebecavsuarez@gmail.com <rebecavsuarez@gmail.com>
2015-09-03 17:06:39 -0400

    AJAx + Json

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ server.setup(function(runningApp) {
   //---- Mounting well-encapsulated application modules
   //---- See: http://vimeo.com/56166857
 
-  runningApp.use('/hello', require('hello')); // attach to sub-route
+  runningApp.use('/', require('hello')); // attach to sub-route
   runningApp.use(require('routes')); // attach to root route
   
   // API endpoint attached to root route:
